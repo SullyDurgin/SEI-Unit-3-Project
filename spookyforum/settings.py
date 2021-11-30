1b63ac54c4de3fce5f079db5bbbb3e5816dfbe47
SullyDurgin <katsdurgin@gmail.com>
2021-11-30 12:22:40 -0500

    set hero and chnage log in path

--- spookyforum/settings.py
+++ spookyforum/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = 'forums_index'
+LOGIN_REDIRECT_URL = 'home'
 
 LOGOUT_REDIRECT_URL = 'home'
 
